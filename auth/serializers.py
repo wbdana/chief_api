91e64ed3bbfd150b725e82f353dd7a79e1f68e2f
wbdana <william.b.dana@gmail.com>
2019-04-21 09:32:08 -0400

    Implement serializers for Owner, Collaborator, Reader models

---
+++ auth/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+from auth.models import Owner, Collaborator, Reader, GithubUser
+from django.contrib.auth.models import User
+from records.serializers import RecordSerializer
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.HyperlinkedRelatedField(many=False, view_name='owner-detail', read_only=True)
+    collaborator = serializers.HyperlinkedRelatedField(many=False, view_name='collaborator-detail', read_only=True)
+    reader = serializers.HyperlinkedRelatedField(many=False, view_name='reader-detail', read_only=True)
+    github_user = serializers.HyperlinkedRelatedField(many=False, view_name='githubuser-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = (
+            'url',
+            'id',
+            'username',
+            'owner',
+            'collaborator',
+            'reader',
+            'github_user',
+        )
+
+
+class OwnerSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.HyperlinkedRelatedField(view_name='user-detail', read_only=True)
+    records = RecordSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Owner
+        fields = (
+            'url',
+            'id',
+            'user',
+            'records',
+        )
+
+
+class CollaboratorSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.HyperlinkedRelatedField(view_name='user-detail', read_only=True)
+    records = RecordSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Collaborator
+        fields = (
+            'url',
+            'id',
+            'user',
+            'records',
+        )
+
+class ReaderSerializer(serializers.HyperlinkedModelSerializer):
+    user = serializers.HyperlinkedRelatedField(view_name='user-detail', read_only=True)
+    records = RecordSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Reader
+        fields = (
+            'url',
+            'id',
+            'user',
+            'records',
+        )
+
